6d4e87d23e4fbb8e7658fe70f23476df2bdd483c
truong <truong263111@gmail.com>
2023-10-07 19:53:17 +0700

    add goto button, update shop view

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,12 +9,36 @@ productList.forEach(product => {
     newProduct.classList.add("list-product__item");
     let productBlock = document.createElement('div');
     productBlock.classList.add("list-product__item__block");
+
     productBlock.style.backgroundImage = `url('${product.image}')`;
     newProduct.appendChild(productBlock);
     productBlock.innerHTML = `
-        <h2>${product.name}</h2>
-        <div class="price">${product.price}</div>
+        <div class="list-product__item-price">$${product.price}</div>
+        <h2 class="list-product__item-name">${product.name}</h2>
     `;
     listProduct.appendChild(newProduct);
 });
 
+
+// to the top, to the bottom buttons
+let btn = document.querySelector('.goto-btn');
+window.onscroll = function() {
+    if (document.documentElement.scrollTop > 100) {
+        btn.style.transform = 'rotate(0deg)';
+        btn.onclick = topFunction;
+    } else {
+        btn.style.transform = 'rotate(180deg)';
+        btn.onclick = botFunction;
+    }
+};
+
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+function botFunction() {
+    document.body.scrollTop = document.body.scrollHeight;
+    document.documentElement.scrollTop = document.documentElement.scrollHeight;
+}
